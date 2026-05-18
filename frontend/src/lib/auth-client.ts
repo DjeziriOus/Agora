93e919cb1da745afd3d9527ead28b8479e2407f5
XeedOus <100945558+DjeziriOus@users.noreply.github.com>
2026-05-18 22:25:12 +0200

    pointing the betterAuth to the frontend now so that brave doesnt trigger by seeing a diffrent URL

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -3,9 +3,12 @@ import { API_URL } from "@/config";
 
 export const authClient = createAuthClient({
   // baseURL: "http://localhost:5001",
-    baseURL: API_URL,
-    fetchOptions: {
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : API_URL,
+  fetchOptions: {
     headers: {
-      "ngrok-skip-browser-warning": "true" 
-    }
-  }});
+      "ngrok-skip-browser-warning": "true",
+    },
+  },
+});
